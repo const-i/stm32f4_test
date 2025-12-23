d89ed9bbae85e54ce0ddca72c57a709ae8e31fdd
const-i <64656750+const-i@users.noreply.github.com>
2025-12-23 17:34:53 +0000

    Add files via upload

---
+++ blink_05/README.md
@@ -0,0 +1,5 @@
+# Blink - Embassy
+
+Blink example of the Embassy framework: https://github.com/embassy-rs/embassy
+
+Code shamelessly taken from: https://github.com/embassy-rs/embassy/tree/main/examples/stm32f4
---
+++ blink_05/src/main.rs
@@ -0,0 +1,29 @@
+#![no_main]
+#![no_std]
+
+use defmt::*;
+use {defmt_rtt as _, panic_probe as _};
+
+use embassy_executor::Spawner;
+use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_time::Timer;
+
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    info!("Hello World!");
+    
+    let p = embassy_stm32::init(Default::default());
+    let mut led = Output::new(p.PD14, Level::High, Speed::Low);
+
+	loop {
+		
+        info!("high");
+        led.set_high();
+        Timer::after_millis(300).await;
+
+        info!("low");
+        led.set_low();
+        Timer::after_millis(300).await;
+	}
+}
