cc5b2441eedc813f1277e12c82528665ee8703dc
const-i <64656750+const-i@users.noreply.github.com>
2025-12-24 19:58:13 +0000

    Add files via upload

---
+++ dac_01/README.md
@@ -0,0 +1,5 @@
+# DAC - Embassy
+
+DAC example of the Embassy framework: https://github.com/embassy-rs/embassy
+
+Code shamelessly taken from: https://github.com/embassy-rs/embassy/tree/main/examples/stm32f4
---
+++ dac_01/src/main.rs
@@ -0,0 +1,38 @@
+#![no_main]
+#![no_std]
+
+use defmt::*;
+use {defmt_rtt as _, panic_probe as _};
+
+use embassy_executor::Spawner;
+use embassy_stm32::dac::{DacCh1, Value};
+
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_stm32::init(Default::default());
+    info!("Hello World!");
+    
+    let mut dac = DacCh1::new_blocking(p.DAC1, p.PA4);
+
+    loop {
+        for v in 0..=255 {
+            dac.set(Value::Bit8(to_sine_wave(v)));
+        }
+    }
+}
+
+
+use micromath::F32Ext;
+
+fn to_sine_wave(v: u8) -> u8 {
+    if v >= 128 {
+        // top half
+        let r = 3.14 * ((v - 128) as f32 / 128.0);
+        (r.sin() * 128.0 + 127.0) as u8
+    } else {
+        // bottom half
+        let r = 3.14 + 3.14 * (v as f32 / 128.0);
+        (r.sin() * 128.0 + 127.0) as u8
+    }
+}
